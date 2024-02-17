680f2d8f4364c937c9b5bb8eb62cf30ea8b91329
Chirag <cva19@sfu.ca>
2024-02-17 11:58:13 -0800

    Refactor action sampling and expert action retrieval in BCAgent

--- a1/bc_agent.py
+++ a1/bc_agent.py
@@ -118,13 +118,18 @@ class BCAgent(base_agent.BaseAgent):
 
         ## a) sample an action from the policy
         # placeholder
-        a_space = self._env.get_action_space()
-        a = torch.zeros(a_space.shape, device=self._device)
+        # a_space = self._env.get_action_space()
+        # a = torch.zeros(a_space.shape, device=self._device)
+        norm_obs = self._obs_norm.normalize(obs)
+        a_dist = self._model.eval_actor(norm_obs)
+        normal_a = a_dist.sample()
+        a = self._a_norm.unnormalize(normal_a).detach()
         
         ## b) query the expert for an action
         # placeholder
-        a_space = self._env.get_action_space()
-        expert_a = torch.zeros(a_space.shape, device=self._device)
+        # a_space = self._env.get_action_space()
+        # expert_a = torch.zeros(a_space.shape, device=self._device)
+        expert_a = self._eval_expert(obs).detach()
 
         a_info = {
             "expert_a": expert_a
@@ -136,5 +141,8 @@ class BCAgent(base_agent.BaseAgent):
         TODO 1.2: Implement code to calculate the loss for training the policy.
         '''
         # placeholder
-        loss = torch.zeros(1)
+        norm_action_dist = self._model.eval_actor(norm_obs)
+        loss = - norm_action_dist.log_prob(norm_expert_a).mean()
+
+        # loss = torch.zeros(1)
         return loss
